6830981a87e6a788e389e8c615fc33979166af1b
Tomás Sáez <tomas.saez2101@alumnos.ubiobio.cl>
2024-10-25 19:51:38 -0300

    ADD product CRUD pt1

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,11 +2,13 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import productRoute from "./product.routes.js";
 
 const router = Router();
 
 router
     .use("/auth", authRoutes)
-    .use("/user", userRoutes);
+    .use("/user", userRoutes)
+    .use("/product",productRoute);
 
 export default router;
