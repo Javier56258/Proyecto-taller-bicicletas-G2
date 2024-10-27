cb4a018b68fe144cb659c959ce19e3dfb7feab0c
Javier Santander <javier.santander2101@alumnos.ubiobio.cl>
2024-10-27 17:23:04 -0300

    Adaptar creacion de reservas y agregar obtencion de estas por fechas seleccionadas por el usuario

--- backend/src/services/reserva.service.js
+++ backend/src/services/reserva.service.js
@@ -7,6 +7,10 @@ import { AppDataSource } from "../config/configDb.js";
 //import { formatToLocalTime } from '../utils/formatDate.js'
 
 export async function createReservaService(dataReserva) {
+    dataReserva.hora = await dataReserva.hora;
+    if (dataReserva.hora.error) {
+        return { error: dataReserva.hora.error };
+    }
     try {
         const reservaRepository = AppDataSource.getRepository(Reserva);   
 
@@ -24,4 +28,22 @@ export async function createReservaService(dataReserva) {
     } catch (error) {
         console.error("Error al crear una reserva: ", error);
     }
+}
+
+//tal vez agregar un tercer paramentro filtro que puede estar nulo
+export async function getReservasService(fechaInicio, fechaFin) { 
+    try {
+        const reservaRepository = AppDataSource.getRepository(Reserva);
+        
+        const reservas = await reservaRepository.createQueryBuilder("reserva")
+      .where("reserva.fecha BETWEEN :fechaInicio AND :fechaFin", { fechaInicio, fechaFin })
+      .getMany();
+
+        if (!reservas || reservas.length === 0) return [null, "No hay reservas"];
+        return[reservas, null];
+    }catch (error) {
+        console.error("Error al obtener las reservas:", error);
+        return [null, "Error interno del servidor"];
+    }
+
 }
