d3488e06c61a8206fc0a623952c28446f68120a4
pipe <felipe.romero2101@alumnos.ubiobio.cl>
2024-10-29 01:01:18 -0300

    ADD CRUD proveedores

---
+++ backend/src/entity/proveedor.entity.js
@@ -0,0 +1,64 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+const ProveedorSchema = new EntitySchema({
+    name: "proveedor",
+    tableName: "proveedores",
+    columns: {
+        idProveedor: {
+            type: "int",
+            primary: true,
+            generated: true
+        },
+        nombreProveedor: {
+            type: "varchar",
+            length: 255,
+            nullable: false,
+        },
+        productos_suministrados: {
+            type: "varchar",
+            length: 255,
+        },
+        PaginaWeb: {
+            type: "varchar",
+            length: 255,
+        },
+        telefono: {
+            type: "varchar",
+            length: 15,
+        },
+        email: {
+            type: "varchar",
+            length: 255,
+        },
+        direccion: {
+            type: "varchar",
+            length: 255,
+        },
+        createdAt: {
+            type: "timestamp with time zone",
+            default: () => "CURRENT_TIMESTAMP",
+            nullable: false,
+        },
+        updatedAt: {
+            type: "timestamp with time zone",
+            default: () => "CURRENT_TIMESTAMP",
+            onUpdate: "CURRENT_TIMESTAMP",
+            nullable: false,
+        }
+    },
+    /*indices: [
+        {
+            name: "IDX_PROVEEDOR",
+            columns: ["idProveedor"],
+            unique: true,
+        },
+        {
+            name: "IDX_PROVEEDOR_EMAIL",
+            columns: ["email"],
+            unique: true,
+        },
+    ],*/
+});
+  
+export default ProveedorSchema;
