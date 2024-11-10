f08c1d1d7969a2e7887ef194a3700b57a456b5ea
Javier Santander <javier.santander2101@alumnos.ubiobio.cl>
2024-11-10 19:24:36 -0300

    Se agrega entidad de horarios para que agregue el dueño y conectar con reservas

---
+++ backend/src/entity/horario.entity.js
@@ -0,0 +1,39 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+//CREACION DE TABLA PRODUCTOS
+const ProductSchema = new EntitySchema({
+    name: "Horario",
+    tableName: "horarios",
+    columns: {
+        id: {
+            type : "int",
+            primary: true,
+            generated: true,
+            unique: true,
+        },
+        dia: {
+            type: "varchar",
+            length: 50,
+            nullable: false,
+        },
+        hora: {
+            type: "varchar",
+            length: 10,
+            nullable: true
+        },
+        createdAt: {
+            type: "timestamp with time zone",
+            default: ()=> "CURRENT_TIMESTAMP",
+            nullable: false,
+        },
+        updatedAt: {
+            type: "timestamp with time zone",
+            default: ()=> "CURRENT_TIMESTAMP",
+            onUpdate: "CURRENT_TIMESTAMP",
+            nullable: false,
+        }
+    }
+});
+
+export default ProductSchema;
