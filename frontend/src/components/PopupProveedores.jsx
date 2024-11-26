b0b14dd179669439b095275a31b15c7fc3b99e55
PabloSanchezMunoz <pablo_2002_@live.com>
2024-11-26 04:16:23 -0300

    MOD FRONT de Form y Table de proovedor

--- frontend/src/components/PopupProveedores.jsx
+++ frontend/src/components/PopupProveedores.jsx
@@ -1,98 +1,101 @@
-import Form from './Form';
-import '@styles/Popup.css';
-import CloseIcon from '@assets/XIcon.svg';
-
+import Form from "./Form";
+import "@styles/Popup.css";
+import CloseIcon from "@assets/XIcon.svg";
 
 function PopupProveedores({ show, setShow, data, action }) {
-    const proveedorData = data && data.length > 0 ? data[0] : {};
+  const proveedorData = data && data.length > 0 ? data[0] : {};
 
-    const handleSubmit = (formData) => {
-        action(formData);
-    }
+  const handleSubmit = (formData) => {
+    action(formData);
+  };
 
-    return (
-        <div>
-            { show && (
-                <div className="bg">
-                <div className="popup">
-                    <button className='close' onClick={() => setShow(false)}>
-                        <img src={CloseIcon} />
-                    </button>
-                    <Form
-                        title="Editar Proveedor"
-                        fields={[
-                            {
-                                label: "Nombre Proveedor",
-                                name: "nombreProveedor",
-                                defaultValue: proveedorData.nombreProveedor || "",
-                                placeholder: 'Ingresar Nombre',
-                                fieldType: 'input',
-                                type: "text",
-                                required: true,
-                                minLength: 5,
-                                maxLength: 50,
-                                pattern: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s]+$/,
-                                patternMessage: "",
-                            },
-                            {
-                                label: "Pagina Web",
-                                name: "PaginaWeb",
-                                defaultValue: proveedorData.PaginaWeb || "",
-                                placeholder: 'www.ejemplo.com',
-                                fieldType: 'input',
-                                type: "text",
-                                pattern: "",
-                                patternMessage: "",
-                                required: false,
-                            },
-                            {
-                                label: "Correo electrónico",
-                                name: "email",
-                                defaultValue: proveedorData.email || "",
-                                placeholder: 'ejemplo@gmail.cl',
-                                fieldType: 'input',
-                                type: "email",
-                                minLength: 10,
-                                maxLength: 30,
-                                required: true,
-                                pattern: /^[a-zA-Z0-9._%+-]+@gmail\.cl$/,
-                                patternMessage: "El correo electrónico debe finalizar en @gmail.cl.",
-                            },
-                            {
-                                label: "Número de teléfono",
-                                name: "telefono",
-                                defaultValue: proveedorData.telefono || "",
-                                placeholder: '+56912345678',
-                                fieldType: 'input',
-                                type: "text",
-                                minLength: 8,
-                                maxLength: 8,
-                                required: true,
-                                pattern: /^\d+$/,
-                                patternMessage: "El teléfono debe contener solo números."
-                            },
-                            
-                            {
-                                label: "Dirección",
-                                name: "direccion",
-                                defaultValue: proveedorData.direccion || "",
-                                placeholder: 'Ingresar Dirección',
-                                fieldType: 'input',
-                                type: "text",
-                                minLength: 5,
-                                maxLength: 50,
-                                required: false,
-                            },
-                        ]}
-                        onSubmit={handleSubmit}
-                        buttonText="Editar proveedor"
-                        backgroundColor={'#fff'}
-                    />
-                </div>
+  return (
+    <div>
+      {show && (
+        <div className="bg">
+          <div className="prov-popup">
+            <button className="close" onClick={() => setShow(false)}>
+              <img src={CloseIcon} />
+            </button>
+            <h1 className="h1-form">Editar Proveedor</h1>
+            <div className="proovedores-form-container">
+              <Form
+                title=""
+                fields={[
+                  {
+                    label: "Nombre Proveedor",
+                    name: "nombreProveedor",
+                    defaultValue: proveedorData.nombreProveedor || "",
+                    placeholder: "Ingresar Nombre",
+                    fieldType: "input",
+                    type: "text",
+                    required: true,
+                    minLength: 5,
+                    maxLength: 50,
+                    pattern: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s]+$/,
+                    patternMessage: "",
+                  },
+                  {
+                    label: "Pagina Web",
+                    name: "PaginaWeb",
+                    defaultValue: proveedorData.PaginaWeb || "",
+                    placeholder: "www.ejemplo.com",
+                    fieldType: "input",
+                    type: "text",
+                    pattern: "",
+                    patternMessage: "",
+                    required: false,
+                  },
+                  {
+                    label: "Correo electrónico",
+                    name: "email",
+                    defaultValue: proveedorData.email || "",
+                    placeholder: "ejemplo@gmail.cl",
+                    fieldType: "input",
+                    type: "email",
+                    minLength: 10,
+                    maxLength: 30,
+                    required: true,
+                    pattern: /^[a-zA-Z0-9._%+-]+@gmail\.cl$/,
+                    patternMessage:
+                      "El correo electrónico debe finalizar en @gmail.cl.",
+                  },
+                  {
+                    label: "Número de teléfono",
+                    name: "telefono",
+                    defaultValue: proveedorData.telefono || "",
+                    placeholder: "+56912345678",
+                    fieldType: "input",
+                    type: "text",
+                    minLength: 8,
+                    maxLength: 8,
+                    required: true,
+                    pattern: /^\d+$/,
+                    patternMessage: "El teléfono debe contener solo números.",
+                  },
+
+                  {
+                    label: "Dirección",
+                    name: "direccion",
+                    defaultValue: proveedorData.direccion || "",
+                    placeholder: "Ingresar Dirección",
+                    fieldType: "input",
+                    type: "text",
+                    minLength: 5,
+                    maxLength: 50,
+                    required: false,
+                  },
+                ]}
+                onSubmit={handleSubmit}
+                buttonText="Editar proveedor"
+                backgroundColor={"#fff"}
+              />
             </div>
-            )}
+          </div>
         </div>
-    );
-} 
+      )}
+    </div>
+  );
+}
 
 export default PopupProveedores;
--- frontend/src/pages/Proveedores.jsx
+++ frontend/src/pages/Proveedores.jsx
@@ -1,101 +1,133 @@
-import Table from '@components/Table';
-import useProveedores from '@hooks/proveedores/useGetProveedores.jsx';
-import Search from '../components/Search.jsx';
-import PopupProveedores from '../components/PopupProveedores.jsx';
-import CreateProveedor from '../components/CreateProveedor.jsx';
-import DeleteIcon from '../assets/deleteIcon.svg';
-import UpdateIcon from '../assets/updateIcon.svg';
-import UpdateIconDisable from '../assets/updateIconDisabled.svg';
-import DeleteIconDisable from '../assets/deleteIconDisabled.svg';
-import { useCallback, useState } from 'react';
-import useEditProveedor from '@hooks/proveedores/useEditProveedor';
-import useDeleteProveedor from '@hooks/proveedores/useDeleteProveedor';
-import '@styles/proveedor.css';
-
+import Table from "@components/Table";
+import useProveedores from "@hooks/proveedores/useGetProveedores.jsx";
+import Search from "../components/Search.jsx";
+import PopupProveedores from "../components/PopupProveedores.jsx";
+import CreateProveedor from "../components/CreateProveedor.jsx";
+import DeleteIcon from "../assets/deleteIcon.svg";
+import UpdateIcon from "../assets/updateIcon.svg";
+import UpdateIconDisable from "../assets/updateIconDisabled.svg";
+import DeleteIconDisable from "../assets/deleteIconDisabled.svg";
+import { useCallback, useState } from "react";
+import useEditProveedor from "@hooks/proveedores/useEditProveedor";
+import useDeleteProveedor from "@hooks/proveedores/useDeleteProveedor";
+import "@styles/proveedor.css";
 
 const Proveedores = () => {
-    const { proveedores, fetchProveedores, setProveedores } = useProveedores();
-    const [filterNombre, setFilterNombre] = useState('');
-    const [isCreatePopupOpen, setIsCreatePopupOpen] = useState(false);
+  const { proveedores, fetchProveedores, setProveedores } = useProveedores();
+  const [filterNombre, setFilterNombre] = useState("");
+  const [isCreatePopupOpen, setIsCreatePopupOpen] = useState(false);
 
+  const {
+    handleClickUpdate,
+    handleUpdate,
+    isPopupOpen,
+    setIsPopupOpen,
+    dataProveedor,
+    setDataProveedor,
+  } = useEditProveedor(setProveedores, fetchProveedores);
 
+  const { handleDelete } = useDeleteProveedor(
+    fetchProveedores,
+    setDataProveedor
+  );
 
-    const {
-        handleClickUpdate,
-        handleUpdate,
-        isPopupOpen,
-        setIsPopupOpen,
-        dataProveedor,
-        setDataProveedor
-    } = useEditProveedor(setProveedores, fetchProveedores);
+  const handleNombreFilterChange = (e) => {
+    setFilterNombre(e.target.value);
+  };
 
+  const handleCreateClick = () => {
+    setIsCreatePopupOpen(true);
+  };
 
-    const { handleDelete } = useDeleteProveedor(fetchProveedores, setDataProveedor);
+  const handleSelectionChange = useCallback(
+    (selectedProveedores) => {
+      setDataProveedor(selectedProveedores);
+    },
+    [setDataProveedor]
+  );
 
-    const handleNombreFilterChange = (e) => {
-        setFilterNombre(e.target.value);
-    };
+  const columns = [
+    { title: "ID", field: "idProveedor", width: 50, responsive: 0 },
+    { title: "Nombre", field: "nombreProveedor", width: 200, responsive: 0 },
+    {
+      title: "Productos Suministrados",
+      field: "productos_suministrados",
+      width: 200,
+      responsive: 2,
+    },
+    { title: "Página Web", field: "PaginaWeb", width: 200, responsive: 2 },
+    { title: "Teléfono", field: "telefono", width: 100, responsive: 2 },
+    { title: "Correo electrónico", field: "email", width: 250, responsive: 3 },
+    { title: "Dirección", field: "direccion", width: 200, responsive: 2 },
+  ];
 
-    const handleCreateClick = () => {
-        setIsCreatePopupOpen(true);
-    };
-
-    const handleSelectionChange = useCallback((selectedProveedores) => {
-        setDataProveedor(selectedProveedores);
-    }, [setDataProveedor]);
-
-   
-    const columns = [
-        { title: "ID", field: "idProveedor", width: 50, responsive: 0 },
-        { title: "Nombre", field: "nombreProveedor", width: 200, responsive: 0 },
-        { title: "Productos Suministrados", field: "productos_suministrados", width: 200, responsive: 2 },
-        { title: "Página Web", field: "PaginaWeb", width: 200, responsive: 2 },
-        { title: "Teléfono", field: "telefono", width: 100, responsive: 2 },        
-        { title: "Correo electrónico", field: "email", width: 250, responsive: 3 },
-        { title: "Dirección", field: "direccion", width: 200, responsive: 2 }
-    ];
-
-    return (
-        <div className='main-container'>
-            <div className='table-container'>
-                <div className='top-table'>
-                    <h1 className='title-table'>Proveedores</h1>
-                    <div className='filter-actions'> 
-                        <Search value={filterNombre} onChange={handleNombreFilterChange} placeholder={'Filtrar por nombre'} />
-                        <button onClick={handleCreateClick} className='create-proveedor-button'>
-                            Añadir Proveedor
-                        </button>
-                        <button className= 'edit-proveedor-button' onClick={handleClickUpdate} disabled={dataProveedor.length === 0}>
-                            {dataProveedor.length === 0 ? (
-                                <img src={UpdateIconDisable} alt="edit-disabled" />
-                            ) : (
-                                <img src={UpdateIcon} alt="edit" />
-                            )}
-                        </button>
-                        <button className='delete-proveedor-button' disabled={dataProveedor.length === 0} onClick={() => handleDelete(dataProveedor)}>
-                            {dataProveedor.length === 0 ? (
-                                <img src={DeleteIconDisable} alt="delete-disabled" />
-                            ) : (
-                                <img src={DeleteIcon} alt="delete" />
-                            )}
-                        </button>
-                        
-                    </div>
-                </div>
-                <Table
-                    data={proveedores}
-                    columns={columns}
-                    filter={filterNombre}
-                    dataToFilter={'nombreProveedor'}
-                    initialSortName={'nombreProveedor'}
-                    onSelectionChange={handleSelectionChange}
-                />
-            </div>
-            <PopupProveedores show={isPopupOpen} setShow={setIsPopupOpen} data={dataProveedor} action={handleUpdate}  />
-            <CreateProveedor show={isCreatePopupOpen} setShow={setIsCreatePopupOpen} action={fetchProveedores} />
-       
+  return (
+    <div className="main-container">
+      <div>
+        <div className="top-table">
+          <div>
+            <h1 className="title-table">Proveedores</h1>
+          </div>
+          <div className="filter-actions">
+            <Search
+              value={filterNombre}
+              onChange={handleNombreFilterChange}
+              placeholder={"Filtrar por nombre"}
+            />
+            <button
+              onClick={handleCreateClick}
+              className="create-proveedor-button"
+            >
+              Añadir Proveedor
+            </button>
+            <button
+              className="edit-proveedor-button"
+              onClick={handleClickUpdate}
+              disabled={dataProveedor.length === 0}
+            >
+              {dataProveedor.length === 0 ? (
+                <img src={UpdateIconDisable} alt="edit-disabled" />
+              ) : (
+                <img src={UpdateIcon} alt="edit" />
+              )}
+            </button>
+            <button
+              className="delete-proveedor-button"
+              disabled={dataProveedor.length === 0}
+              onClick={() => handleDelete(dataProveedor)}
+            >
+              {dataProveedor.length === 0 ? (
+                <img src={DeleteIconDisable} alt="delete-disabled" />
+              ) : (
+                <img src={DeleteIcon} alt="delete" />
+              )}
+            </button>
+          </div>
         </div>
-    );
+        <div>
+          <Table
+            data={proveedores}
+            columns={columns}
+            filter={filterNombre}
+            dataToFilter={"nombreProveedor"}
+            initialSortName={"nombreProveedor"}
+            onSelectionChange={handleSelectionChange}
+          />
+        </div>
+      </div>
+      <PopupProveedores
+        show={isPopupOpen}
+        setShow={setIsPopupOpen}
+        data={dataProveedor}
+        action={handleUpdate}
+      />
+      <CreateProveedor
+        show={isCreatePopupOpen}
+        setShow={setIsCreatePopupOpen}
+        action={fetchProveedores}
+      />
+    </div>
+  );
 };
 
 export default Proveedores;
